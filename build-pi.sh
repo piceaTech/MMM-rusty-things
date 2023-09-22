737afa27b481dc7bd17c468da085071833dd0956
Felix Fichte <dev@spruce.de>
2023-09-22 17:22:05 +0200

    Fix: More verbose build logging

--- build-pi.sh
+++ build-pi.sh
@@ -1,9 +1,10 @@
 RUSTFLAGS="-L $PWD/pi_deps/usr/lib/gcc/arm-linux-gnueabihf/12/" \
-CC=armv7-unknown-linux-gnueabihf-cc \
+CC="armv7-unknown-linux-gnueabihf-cc" \
 PKG_CONFIG_ALLOW_CROSS=true \
 OPENSSL_INCLUDE_DIR="$PWD/pi_deps/usr/include" \
 OPENSSL_LIB_DIR="$PWD/pi_deps/usr/lib/arm-linux-gnueabihf" \
 RUST_BACKTRACE=1 \
 CARGO_BUILD_TARGET="arm-unknown-linux-gnueabihf" \
+CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG=true \
 npm_config_arch=arm \
 npm run build-release
